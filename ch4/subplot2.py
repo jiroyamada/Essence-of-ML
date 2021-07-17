a50e2b13835675a021e555f8d4bc09d31c06f7d3
jiroyamada <poirot343@outlook.jp>
2021-07-18 05:12:10 +0900

    複数グラフ2

---
+++ ch4/subplot2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-5,5,300)
+y = x**2
+
+fig, ax = plt.subplots()
+ax.plot(x,y, color="r")
+plt.show()
